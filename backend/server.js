784d552ba031e64f435f789ba8e22a95a47ae5d8
gunal-01 <gunalprakash006@gmail.com>
2024-11-03 23:26:36 +0530

    updateServer.js1

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,17 @@ app.post("/api/products", async (req, res) => {  // this app.post("/products, as
     }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted"});
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product Not Found"});
+    }
+});
+
 app.listen(5000, () => {
     connectDB();
     console.log("Server started at http://localhost:5000");
