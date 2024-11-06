a98ea28794d623bf94d672fbbc7702dc0596d4aa
gunal-01 <gunalprakash006@gmail.com>
2024-11-06 08:53:27 +0530

    credUpdate1.2

--- backend/server.js
+++ backend/server.js
@@ -2,56 +2,16 @@
 import express from 'express';  // here we imported express through changing the [pkg.json] file by adding ["type": 'module';]
 import dotenv from "dotenv"; // imports for allowing us to load environment variables from a .env file into process.env
 import { connectDB } from './config/db.js'; //manages the connection to MongoDB database through the function connectDB from another file(db.js)
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js"; // routes
+
 dotenv.config(); // loads env varibles from .env file into process.env. This is necessary to use variables like MONGO_URI securely.
 const app = express();  // creates an instance of an Express application, which will be used to handle HTTP requests and responses
 
 app.use(express.json()); // allows us to accept the JSON data in the req.body [Especially this acts as a **middleware**]
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products })
-    } catch(error) {
-        console.log("error in fetching products:", error.message);
-        res.status(500).json({ success: false, message: "Server Error"});
-    }
-});
-app.post("/api/products", async (req, res) => {  // this app.post("/products, async (req, res) => sets up the route handler for GET request to /products. The handler function is asynchronous to allow for the use of await inside.
-    
-    /*Logic to fetch products from database goes here*/
-
-    const product = req.body; // user will send this data
-
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success:false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in Create Product:", error.message);
-        res.status(500).json({ success: false, message: "Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const {id} = req.params;
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted"});
-    } catch (error) {
-        res.status(404).json({ success: false, message: "Product Not Found"});
-    }
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:5000 ");
 });
-
-//  passwd: O43v6VDPGo9WXhud
